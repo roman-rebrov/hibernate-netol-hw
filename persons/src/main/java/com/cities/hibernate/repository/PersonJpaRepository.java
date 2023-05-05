94d50698ff0bc0cae3cd00c73874cd253f6041c5
Roman <61121402+rebrov-rc@users.noreply.github.com>
2023-05-05 16:18:44 +0300

    update jpa-repository

--- persons/src/main/java/com/cities/hibernate/repository/PersonJpaRepository.java
+++ persons/src/main/java/com/cities/hibernate/repository/PersonJpaRepository.java
@@ -1,6 +1,7 @@
 package com.cities.hibernate.repository;
 
 import com.cities.hibernate.entity.Person;
+import com.cities.hibernate.entity.PersonId;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PersonJpaRepository extends JpaRepository<Person, String> {
+public interface PersonJpaRepository extends JpaRepository<Person, PersonId> {
         public List<Person> findByCity(String city);
         public Optional<Person> findByNameAndSurname(String name, String surname);
         public List<Person> findByAgeLessThan(int age, Sort sort);
