27dd821f5c76c2ac683c810f500e637c927e36ac
Roman <61121402+rebrov-rc@users.noreply.github.com>
2023-05-05 21:15:35 +0300

    upload project

--- persons/src/main/java/com/cities/hibernate/repository/PersonJpaRepository.java
+++ persons/src/main/java/com/cities/hibernate/repository/PersonJpaRepository.java
@@ -2,8 +2,8 @@ package com.cities.hibernate.repository;
 
 import com.cities.hibernate.entity.Person;
 import com.cities.hibernate.entity.PersonId;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +11,10 @@ import java.util.Optional;
 
 @Repository
 public interface PersonJpaRepository extends JpaRepository<Person, PersonId> {
+        @Query("select p from Person as p where p.city = :city")
         public List<Person> findByCity(String city);
+        @Query("select p from Person as p where p.name = :name and p.surname = :surname")
         public Optional<Person> findByNameAndSurname(String name, String surname);
-        public List<Person> findByAgeLessThan(int age, Sort sort);
+        @Query("select p from Person as p where p.age < :age order by p.age")
+        public List<Person> findByAgeLessThan(int age);
 }
--- persons/src/main/java/com/cities/hibernate/repository/PersonRepository.java
+++ persons/src/main/java/com/cities/hibernate/repository/PersonRepository.java
@@ -4,12 +4,10 @@ package com.cities.hibernate.repository;
 import com.cities.hibernate.entity.Person;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 @Repository
 public class PersonRepository {
@@ -17,7 +15,6 @@ public class PersonRepository {
 
     @Autowired
     private PersonJpaRepository jpaRepository;
-    private Logger log = Logger.getLogger(PersonRepository.class.getName());
 
     @Transactional
     public List<Person> getPersonsByCity(String city){
@@ -38,7 +35,7 @@ public class PersonRepository {
     @Transactional
     public List<Person> getPersonsByAge(int age) {
 
-        List<Person> resultList = jpaRepository.findByAgeLessThan(age, Sort.by("age"));
+        List<Person> resultList = jpaRepository.findByAgeLessThan(age);
 
         return resultList;
     }
